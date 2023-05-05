7677e851f5a4bf39ad9943250e74f7a00b14d392
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-05-05 17:24:09 +0545

    bob commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import logo from "./logo.svg";
+import { useEffect } from "react";
 import "./App.css";
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
+
+const WS_URL = "ws://localhost:8000";
 
 function App() {
-  return <div className="App">Bobsi</div>;
+  const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log("WebSocket connection established.");
+    },
+    onMessage: (param) => {
+      console.log("MEssage received", param);
+    },
+  });
+
+  useEffect(() => {
+    console.log({ lastMessage });
+  }, []);
+
+  const messageSendHandler = () => {
+    sendMessage("Hello");
+  };
+
+  return (
+    <div>
+      <button onClick={messageSendHandler}>Send Message</button>
+    </div>
+  );
 }
 
 export default App;
